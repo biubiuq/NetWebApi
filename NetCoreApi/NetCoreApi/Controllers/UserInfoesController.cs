7c8bfe7824a48d1acb3378a465c0c2eef6fe6ea3
biubiuq <495068093@qq.com>
2020-08-24 17:11:18 +0800

    添加了新增方法不报错

--- NetCoreApi/NetCoreApi/Controllers/UserInfoesController.cs
+++ NetCoreApi/NetCoreApi/Controllers/UserInfoesController.cs
@@ -116,7 +116,9 @@ namespace NetCoreApi.Controllers
     // To protect from overposting attacks, enable the specific properties you want to bind to, for
     // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
     [HttpPost]
-    public async Task<ActionResult<UserInfo>> PostUserInfo(UserInfo userInfo)
+    public async Task<ActionResult<UserInfo>> PostUserInfo(
+        [ModelBinder(BinderType = typeof(EntityModelBinder2<UserInfo>))]
+      UserInfo userInfo)
     {
       userInfo.ID = Guid.NewGuid().ToString();
       _context.UserInfo.Add(userInfo);
